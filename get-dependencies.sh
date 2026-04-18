e6a9dcc7503e74a811c12dbc9316e3a6ab394d84
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-04-17 21:21:04 -0400

    trying flow.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,17 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm libxft harfbuzz base-devel
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+# echo "Installing debloated packages..."
+# echo "---------------------------------------------------------------"
+# get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-
+make install st DESTDIR=./build PREFIX=/st_make
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=0.9.3
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=./st.png
+#export DESKTOP=./build/
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./build/st_make/bin/st
 
 # Additional changes can be done in between here
 
