bae66353f1cef8511648d0083eceae497accb99e
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-05-06 22:03:11 -0400

    added get-dependencies with DESTDIR.

---
+++ get-dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm libxft harfbuzz base-devel libxcursor ttf-jetbrains-mono-nerd
+# echo "Installing debloated packages..."
+# echo "---------------------------------------------------------------"
+# get-debloated-pkgs --add-common --prefer-nano
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+make install st DESTDIR=./build
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
