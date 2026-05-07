44516274439ee05174600b9039336911b0985326
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-05-06 22:32:35 -0400

    added correct dependencies, correct x11 libraries from aur pkgbuilds added st.desktop additional info. added xrdb to make-appimage.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libxft harfbuzz base-devel libxcursor ttf-jetbrains-mono-nerd
+pacman -Syu --noconfirm libxft harfbuzz base-devel libxcursor ttf-jetbrains-mono-nerd xorg-xrdb
 # echo "Installing debloated packages..."
 # echo "---------------------------------------------------------------"
 # get-debloated-pkgs --add-common --prefer-nano
@@ -15,7 +15,7 @@ pacman -Syu --noconfirm libxft harfbuzz base-devel libxcursor ttf-jetbrains-mono
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-make install st DESTDIR=./build
+make install st DESTDIR=./build PREFIX=/usr X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
---
+++ make-appimage.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+#VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(sed -n 's/^VERSION = //p' ./config.mk)
+# VERSION=0.9.3
+export ARCH VERSION
+
+DESTDIR=./build
+PREFIX=/usr
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=./st.png
+export DESKTOP=$(DESTDIR)$(PREFIX)/share/applications/st.desktop
+# Deploy dependencies
+echo "Running xrdb merge"
+xrdb -merge ./st_demo.Xresources
+quick-sharun $DESTDIR$PREFIX/bin/st
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
