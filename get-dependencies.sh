bc18c0f50dbf67a6af4baab732a192c6a7299fcf
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-06-14 12:43:46 -0400

    testing left.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,11 +15,20 @@ pacman -Syu --noconfirm libxft harfbuzz base-devel libxcursor ttf-jetbrains-mono
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-make install st DESTDIR=./build PREFIX=/usr X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    # weird but `imlib2` is needed instead of `libsixel` for st to use sixel patch.
+    echo "Nightly release: Installing imlib2 for sixel patch..."
+    echo "---------------------------------------------------------------"
+    pacman -Syu --noconfirm imlib2
+    echo "Applying sixel patch"
+    echo "---------------------------------------------------------------"
+    sed --in-place 's/#define SIXEL_PATCH 0/#define SIXEL_PATCH 1/g' patches.def.h
+    make install st DESTDIR=./build PREFIX=/usr \
+        X11INC=/usr/include/X11 X11LIB=/usr/lib/X11 \
+        SIXEL_C="sixel.c sixel_hls.c" SIXEL_LIBS="$(pkg_config --libs imlib2)"
+else
+    make install st DESTDIR=./build PREFIX=/usr \
+        X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
+fi
--- make-appimage.sh
+++ make-appimage.sh
@@ -15,15 +15,18 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=./st.png
 export DESKTOP=$DESTDIR$PREFIX/share/applications/st.desktop
-# Deploy dependencies
-# Deploy fonts. Hoping right now that this will automatically make
-# sharun and anything else look for this before doing anything.
-# testing pending.
-export DEPLOY_DATADIR=1
-# quick-sharun $DESTDIR$PREFIX/bin/st
+# to avoid false triggers, since st is software rendering only.
+export DEPLOY_OPENGL=0
+export DEPLOY_VULKAN=0
+# Setting runtime to always keep the mount point, should speed up startup.
+export URUNTIME_PRELOAD=1
 # Explicitly adding a path to xrdb binary is better way to ensure sharun packages it.
 quick-sharun $DESTDIR$PREFIX/bin/st /usr/bin/xrdb
 # Additional changes can be done in between here
+# Fonts need to be manually copied over.
+# copying only the monospace, ligature enabled nerd font.
+mkdir --parents ./AppDir/usr/share/fonts/TTF
+find /usr/share/fonts/TTF/ -maxdepth 1 -name "JetBrainsMonoNerdFontMono-*.ttf" -exec cp {} ./AppDir/usr/share/fonts/TTF/ \;
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
